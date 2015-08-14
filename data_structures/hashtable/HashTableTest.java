7f51489f27c9f40535520e5fdb8756af472ba06a
Brian Bechtel <brian.bechtel@openmarket.com>
2015-08-14 14:05:04 -0700

    fixed HashTableTest

--- data_structures/hashtable/HashTableTest.java
+++ data_structures/hashtable/HashTableTest.java
@@ -1,12 +1,13 @@
 public class HashTableTest {
     public static void main(String[] args) {
-        HashTable<String> hashTable = new HashTable<String>();
-        String[] array = {"Tom", "Dick", "Harry"};
-        for (String s : array) {
-            hashTable.insert(s);
+        HashTable<String, Integer> hashTable = new HashTable<>();
+        String[] keys = {"Tom", "Dick", "Harry"};
+        int[] values = {23, 27, 24};
+        for (int i = 0; i < 3; i++) {
+            hashTable.put(keys[i], values[i]);
         }
-        for (String s : array) {
-            System.out.println(hashTable.contains(s));
+        for (String s : keys) {
+            System.out.println(hashTable.get(s));
         }
     }
 }
