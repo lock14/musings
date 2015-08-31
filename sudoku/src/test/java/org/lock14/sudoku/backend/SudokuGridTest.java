5d5c39681b6596f51339a8a8afc094d3646beac1
Brian Bechtel <core.editor>
2015-08-30 21:03:40 -0500

    more whitespace fixing

--- sudoku/src/test/java/org/lock14/sudoku/backend/SudokuGridTest.java
+++ sudoku/src/test/java/org/lock14/sudoku/backend/SudokuGridTest.java
@@ -3,9 +3,9 @@ package org.lock14.sudoku.backend;
 import org.junit.Test;
 
 public class SudokuGridTest {
-	
-	@Test
-	public void test() {
-		
-	}
+    
+    @Test
+    public void test() {
+        
+    }
 }
