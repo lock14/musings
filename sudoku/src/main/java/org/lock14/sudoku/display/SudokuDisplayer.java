c5d04d8d7d7308645cd1539b04cd269071f42347
Brian Bechtel <brianrbechtel@gmail.com>
2016-07-16 14:59:10 -0700

    breaking this out as its own project

--- sudoku/src/main/java/org/lock14/sudoku/display/SudokuDisplayer.java
+++
@@ -1,9 +0,0 @@
-package org.lock14.sudoku.display;
-
-import org.lock14.sudoku.backend.SudokuGrid;
-
-public interface SudokuDisplayer {
-    public void display(SudokuGrid puzzle);
-
-    public void displayMessage(String message); 
-}
