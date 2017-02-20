8b209bb3ed91456d8f3e81d73f5fb4de3545baa0
Brian Bechtel <brianrbechtel@gmail.com>
2017-02-20 11:49:02 -0600

    Removed.
    These files were split off into their own repository.
    New repository: https://github.com/lock14/data_structures

--- data_structures/queue/Queue.java
+++
@@ -1,5 +0,0 @@
-public interface Queue<E> {
-    public void add(E data);
-    public E remove();
-    public boolean isEmpty();
-}
--- data_structures/stack/Stack.java
+++
@@ -1,5 +0,0 @@
-public interface Stack<E> {
-    public void push(E data);
-    public E pop();
-    public boolean isEmpty();
-}
