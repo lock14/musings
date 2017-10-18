3532bb65f5c2a55f717480c8156235c8d89cfdaf
lock14 <lock14@users.noreply.github.com>
2017-10-17 20:33:45 -0500

    Update ProbabilityThirteenInStringLengthN.java

--- prob/thirteen_in_n_digits/ProbabilityThirteenInStringLengthN.java
+++ prob/thirteen_in_n_digits/ProbabilityThirteenInStringLengthN.java
@@ -52,6 +52,15 @@ import java.math.BigDecimal;
  * number occuring in an n digit string so long as the two digits are not the 
  * same. For numbers like '11', '22', etc, the reasoning for replacing 
  * (A intersect B) with (A interssect C) does not work.
+ *
+ * If you are doubting the correctness of this derivation you can compare the output
+ * of the following python function with the output of this program. The python
+ * function computes the proportion of strings that contain a '13' in an n length
+ * string using brute force (e.g. it generates all possible strings and checks
+ * each one). Fair warning it is quite slow as a result of this.
+ * 
+ * def proportion_of_strings_containing_thirteen(n):
+ *     return len([x for x in range(0, 10**n) if "13" in str(x)]) / 10**n
  */
 public class ProbabilityThirteenInStringLengthN {
     public static void main(String[] args) {
