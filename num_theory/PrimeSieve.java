8aee09ebf5602eee5b69c0878b47e131a5d3fe81
lock14 <lock14@users.noreply.github.com>
2018-11-07 01:49:10 -0800

    Update PrimeSieve.java

--- num_theory/PrimeSieve.java
+++ num_theory/PrimeSieve.java
@@ -13,7 +13,7 @@ public final class PrimeSieve {
                         .peek(p -> isPrime.combine(n -> n % p != 0));
     }
 
-    private static class MutableIntPredicate implements IntPredicate {
+    private static final class MutableIntPredicate implements IntPredicate {
         IntPredicate predicate = n -> true;
 
         @Override
