b8613099b9264fd628a28af7fbe61bf555de254e
Brian Bechtel <lock14@users.noreply.github.com>
2022-02-06 17:50:08 -0800

    Update ProbabilityThirteenInStringLengthN.java

--- probability/two-digit-string-in-n-digits/ProbabilityThirteenInStringLengthN.java
+++ probability/two-digit-string-in-n-digits/ProbabilityThirteenInStringLengthN.java
@@ -79,7 +79,7 @@ public class ProbabilityThirteenInStringLengthN {
             int n = Integer.parseInt(args[0]);
             if (n >= 0) {
                 BigDecimal probability = F(n);
-                System.out.printf("True probability %f%n", probability);
+                System.out.println("True probability " + probability);
                 System.out.printf("There is a %.2f%% chance a '13' occurs in string of length %d %n", probability.multiply(BigDecimal.TEN.pow(2)), n);
             } else {
                 System.err.println("Error: argument cannot be less than zero");
